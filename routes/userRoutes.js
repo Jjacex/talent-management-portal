2ab7b80be876c4830ec6d02cdfa5cdb51bcd8c33
Jjacex <jacexecaj@gmail.com>
2022-04-13 17:01:32 -0500

    fixed email problem

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -230,8 +230,8 @@ router.put('/home/reject/:id', async (req, res) => {
     const transporter = nodemailer.createTransport({
         host: 'smtp-mail.outlook.com',
         auth: {
-            user: 'ekkwjsmcnfd2dsdks@outlook.com',
-            pass: 'djrueEdosldk32d', 
+            user: process.env.EMAIL_USER,
+            pass: process.env.EMAIL_PASS, 
         },
     })
     const rejectTemplate = `
